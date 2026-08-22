0ba539e26d957ba00db1f9d772f10d0b78e62699
RohanSaikumar <rohansaikumar05@gmail.com>
2026-08-22 20:41:38 +0530

    Deploy backend with HTTPS

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,8 +1,12 @@
 import dns from "node:dns";
+import https from "node:https";
+import fs from "node:fs";
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-await dns.promises.resolveSrv("_mongodb._tcp.cluster0.vpvf2vb.mongodb.net");
+await dns.promises.resolveSrv(
+    "_mongodb._tcp.cluster0.vpvf2vb.mongodb.net"
+);
 
 import { extractMemory } from "./utils/extractMemories.js";
 import express from "express";
@@ -22,17 +26,15 @@ import ragTestRoutes from "./routes/RAGTest.js";
 import ragExperimentRoutes from "./routes/RAGExperiment.js";
 import transcriptionRoutes from "./routes/transcription.js";
 
-
-
 const app = express();
-const PORT = 8080;
 
-
+const PORT = 443;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://aether-chi-pied.vercel.app",
     credentials: true
 }));
+
 app.use(express.json());
 
 app.use(
@@ -41,38 +43,52 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            maxAge: 1000 * 60 * 60 * 24
+            maxAge: 1000 * 60 * 60 * 24,
+            secure: true,
+            sameSite: "none"
         }
     })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
-
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected with Database!");
-    } catch(err) {
-        console.log("Failed to connect with Db", err);
-    }
-}
-
-await connectDB();
-
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+// Routes
 app.use("/api", chatRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/document", documentRoutes);
 app.use("/api/workspace", workspaceRoutes);
-app.use("/api/evaluation",evaluationRoutes);
+app.use("/api/evaluation", evaluationRoutes);
 app.use("/api/evaluation/tests", ragTestRoutes);
-app.use("/api/evaluation/experiments",ragExperimentRoutes);
+app.use("/api/evaluation/experiments", ragExperimentRoutes);
 app.use("/api/transcription", transcriptionRoutes);
+
+
+// MongoDB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected with Database!");
+    } catch (err) {
+        console.log("Failed to connect with Db", err);
+    }
+};
+
+await connectDB();
+
+
+// SSL certificates
+const sslOptions = {
+    key: fs.readFileSync(
+        "/etc/letsencrypt/live/3-7-168-116.nip.io/privkey.pem"
+    ),
+    cert: fs.readFileSync(
+        "/etc/letsencrypt/live/3-7-168-116.nip.io/fullchain.pem"
+    )
+};
+
+
+// HTTPS server
+https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`HTTPS server is running on port ${PORT}`);
+});
